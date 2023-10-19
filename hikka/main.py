d7f7d76081d6430689366570dece54e4b9d4f937
s1zexxx <135944334+s1zexxx@users.noreply.github.com>
2023-10-19 15:12:23 +0500

    Update main.py

--- hikka/main.py
+++ hikka/main.py
@@ -58,25 +58,6 @@ from .dispatcher import CommandDispatcher
 from .tl_cache import CustomTelegramClient
 from .translations import Translator
 from .version import netver, __version__
-from banner import banner
-from pystyle import *
-
-COLOR_CODE = {
-    "RESET": "\033[0m",  
-    "UNDERLINE": "\033[04m", 
-    "GREEN": "\033[32m",     
-    "YELLOW": "\033[93m",    
-    "RED": "\033[31m",       
-    "CYAN": "\033[36m",     
-    "BOLD": "\033[01m",        
-    "PINK": "\033[95m",
-    "URL_L": "\033[36m",       
-    "LI_G": "\033[92m",      
-    "F_CL": "\033[0m",
-    "DARK": "\033[90m",     
-}
-
-print(Colorate.Horizontal(Colors.red_to_white,Center.XCenter(banner)))
 
 try:
     from .web import core
