59e460b19ba805803b253d3e19772aaa5488633e
s1zexxx <135944334+s1zexxx@users.noreply.github.com>
2023-10-19 14:33:04 +0500

    Update banner.sh

--- assets/banner.sh
+++ assets/banner.sh
@@ -10,13 +10,12 @@ print_center(){
 
 echo -ne "\\033[2J\033[3;1f"
 print_center "
-\033[95m    ___                      _             __\033[0m                
-\033[95m   /   |  ____  ____  ____  (_)___  ____ _/ /_____  _____\033[0m    
-\033[95m  / /| | / __ \/ __ \/ __ \/ /_  / / __ `/ __/ __ \/ ___/\033[0m    
-\033[95m/ ___ |/ / / / /_/ / / / / / / /_/ /_/ / /_/ /_/ / /\033[0m        
-\033[95m/_/  |_/_/ /_/\____/_/ /_/_/ /___/\__,_/\__/\____/_/\033[0m         
-                                                             
-                                                         
-\033[95mAnonizator started successfully!\033[0m
+\033[95m|Adaptator Adaptator Adaptator Adaptator|033[0m
+\033[95m|Adaptator Adaptator Adaptator Adaptator|033[0m
+\033[95m|Adaptator Adaptator Adaptator Adaptator|033[0m
+\033[95m|Adaptator Adaptator Adaptator Adaptator|033[0m
+\033[95m|Adaptator Adaptator Adaptator Adaptator|033[0m
+
+\033[95mAdaptator started successfully!\033[0m
 \033[95mWeb url: http://localhost:1242\033[0m
 "
